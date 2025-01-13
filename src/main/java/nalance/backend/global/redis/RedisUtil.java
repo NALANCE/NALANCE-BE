59d08e0d96a40c7f70c52608ddfdbbd1bddaffe1
dabeann <dabin4078@daum.net>
2025-01-13 23:18:18 +0900

    🔧  Feature : redis config

---
+++ src/main/java/nalance/backend/global/redis/RedisUtil.java
@@ -0,0 +1,25 @@
+package nalance.backend.global.redis;
+
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtil {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void saveEmailVerificationCode(String email, String code) {
+        redisTemplate.opsForValue().set(email, code, 5, TimeUnit.MINUTES);
+    }
+
+    public String getEmailVerificationCode(String email) {
+        return redisTemplate.opsForValue().get(email);
+    }
+
+    public void deleteEmailVerificationCode(String email) {
+        redisTemplate.delete(email);
+    }
+}
