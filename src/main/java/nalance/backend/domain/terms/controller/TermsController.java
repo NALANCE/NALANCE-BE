2c0cc5e4fcedd1547cba27ed4d25626187067e74
Yewon Yoo <yooyy0106@naver.com>
2025-01-16 03:24:18 +0900

    :sparkles: Feature : 약관 컨트롤러에 에러코드 추가

--- src/main/java/nalance/backend/domain/terms/controller/TermsController.java
+++ src/main/java/nalance/backend/domain/terms/controller/TermsController.java
@@ -43,7 +43,8 @@ public class TermsController {
     @Operation(summary = "약관 개별 조회 API", description = "특정 약관의 세부 정보를 조회하는 API입니다.")
     @ApiResponses({
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "TERMS4002", description = "약관 개별 조회에 실패했습니다.")
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "TERMS4002", description = "잘못된 형식의 약관 ID 입니다."),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "TERMS4003", description = "해당 약관이 존재하지 않습니다.")
     })
     public ApiResponse<TermsDetailResponse> getTermsById(@PathVariable Long termsId) {
         TermsDetailResponse terms = termsQueryService.getTermsById(termsId);
