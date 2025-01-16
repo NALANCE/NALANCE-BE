ed616ba90c2cd59fd8127570a8af1b35441955a2
Kang Chaewon <kang960953@gmail.com>
2025-01-16 19:08:34 +0900

    ✨  Feat : 카테고리 생성/수정 시 중복 카테고리명 예외처리
     Feat : 카테고리 생성/수정 시 중복 카테고리명 예외처리

--- src/main/java/nalance/backend/domain/category/service/impl/CategoryCommandServiceImpl.java
+++ src/main/java/nalance/backend/domain/category/service/impl/CategoryCommandServiceImpl.java
@@ -17,8 +17,12 @@ import java.util.stream.Collectors;
 @Transactional
 public class CategoryCommandServiceImpl implements CategoryCommandService {
     private final CategoryRepository categoryRepository;
+    // Todo : memberId 추후 수정 -> createOneCategory, createManyCateory
+    Long memberId = 1L;
     @Override
     public void createOneCateory(CategoryDTO.CategoryRequest categoryRequest) {
+        // Valid : 멤버의 기존 카테고리 이름 중복여부 확인
+        validateCategoryNames(List.of(categoryRequest.getCategoryName()), memberId);
         Category category = Category.builder()
                         .categoryName(categoryRequest.getCategoryName())
                         .color(categoryRequest.getColor())
@@ -30,6 +34,14 @@ public class CategoryCommandServiceImpl implements CategoryCommandService {
 
     @Override
     public void createManyCateory(List<CategoryDTO.CategoryRequest> categoryRequests) {
+        // Valid : 멤버의 기존 카테고리 이름 중복여부 확인
+        List<String> newCategoryNames = categoryRequests.stream()
+                .map(CategoryDTO.CategoryRequest::getCategoryName)
+                .toList();
+
+        if (!categoryRequests.isEmpty()) {
+            validateCategoryNames(newCategoryNames, memberId);
+        }
         // 카테고리 리스트 생성 및 변환
         List<Category> categories = categoryRequests.stream().map(
                 categoryRequest ->  Category.builder()
@@ -47,6 +59,9 @@ public class CategoryCommandServiceImpl implements CategoryCommandService {
     public Category updateCategory(Long memberId, CategoryDTO.CategoryUpdateRequest categoryRequest) {
         Category category = categoryRepository.findByCategoryIdAndMember_MemberId(categoryRequest.getCategoryId(), memberId)
                 .orElseThrow(() -> new CategoryException(ErrorStatus.CATEGORY_NOT_FOUND));
+        // Valid : 멤버의 기존 카테고리 이름 중복여부 확인
+        validateCategoryNames(List.of(categoryRequest.getCategoryName()), memberId);
+
         category.updateCategoryDetails(categoryRequest.getCategoryName(), categoryRequest.getColor());
         return categoryRepository.save(category);
     }
@@ -58,5 +73,22 @@ public class CategoryCommandServiceImpl implements CategoryCommandService {
             throw new CategoryException(ErrorStatus.CATEGORY_NOT_FOUND);
         }
         categoryRepository.deleteById(categoryId);
+    }
+
+    // 멤버별 중복된 카테고리명 검증 메소드
+    private void validateCategoryNames(List<String> categoryNames, Long memberId) {
+        List<String> existingCategoryNames = categoryRepository.findCategoriesByMember_MemberId(memberId)
+                .stream()
+                .map(Category::getCategoryName)
+                .toList();
+
+        // 중복인지 확인
+        List<String> duplicateNames = categoryNames.stream()
+                .filter(existingCategoryNames::contains)
+                .toList();
+
+        if (!duplicateNames.isEmpty()) {
+            throw new CategoryException(ErrorStatus.CATEGORY_NAME_ALREADY_EXISTS);
+        }
     }
 }
