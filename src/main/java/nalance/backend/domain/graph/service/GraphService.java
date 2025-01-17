27a23958221ae289b9af03711dd52fa44c142356
minkchoii <grace820@dgu.ac.kr>
2025-01-17 22:23:13 +0900

    Feature: 그래프기능구현

--- src/main/java/nalance/backend/domain/graph/service/GraphService.java
+++
@@ -1,8 +0,0 @@
-package nalance.backend.domain.graph.service;
-
-import nalance.backend.domain.graph.dto.GraphDTO.GraphResponse.*;
-
-public interface GraphService {
-    GraphDailyResponse getDailyGraph(String date);
-    CalendarMonthlyResponse getMonthlyCalendar(int year, int month);
-}
