781d65733b24638d891558bf1bc3824ae0df044b
dabeann <dabin4078@daum.net>
2025-02-21 14:25:36 +0900

    ♻️  Refactor : 이메일 검증 정규식으로 변경

--- src/main/java/nalance/backend/domain/member/dto/EmailDTO.java
+++ src/main/java/nalance/backend/domain/member/dto/EmailDTO.java
@@ -2,6 +2,7 @@ package nalance.backend.domain.member.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import nalance.backend.global.validation.annotation.CheckEmailCode;
 
@@ -12,14 +13,14 @@ public class EmailDTO {
         @Getter
         public static class EmailSendVerificationRequest {
             @NotBlank(message = "이메일은 필수 입력 값입니다.")
-            @Email(message = "이메일 형식에 맞지 않습니다.")
+            @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message = "이메일 형식에 맞지 않습니다.")
             private String email;
         }
 
         @Getter
         public static class VerificationEmailCodeRequest {
             @NotBlank(message = "이메일은 필수 입력 값입니다.")
-            @Email(message = "이메일 형식에 맞지 않습니다.")
+            @Pattern(regexp = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+[.][a-zA-Z]{2,3}$", message = "이메일 형식에 맞지 않습니다.")
             private String email;
             @CheckEmailCode
             private String code;
